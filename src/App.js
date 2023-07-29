3691a2f81dde9aa2ae01b6581e45b880229c165a
gmarvis <sgmarvis@gmail.com>
2023-07-29 16:36:08 +0100

    learning react routers indept

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>JobaRouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+
+          <Route />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus modi sit
+        omnis repellat, tempora porro quis libero nemo ratione? Itaque
+        consectetur, rerum incidunt, nisi aliquid blanditiis quidem inventore
+        libero iusto nam neque necessitatibus est odio accusamus magni tempora
+        vero voluptas sunt sed omnis, aliquam qui? Accusamus laboriosam nisi
+        ratione laudantium dicta tenetur eveniet modi provident earum dolor,
+        quidem illum esse pariatur omnis facere, soluta distinctio reiciendis
+        neque. Pariatur recusandae eos eaque est hic magnam quidem eveniet
+        accusantium officia doloremque ut illum adipisci fugit veritatis
+        architecto, culpa, quod asperiores facere molestiae. Delectus dolore
+        deleniti molestiae inventore itaque suscipit nesciunt expedita modi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus modi sit
+        omnis repellat, tempora porro quis libero nemo ratione? Itaque
+        consectetur, rerum incidunt, nisi aliquid blanditiis quidem inventore
+        libero iusto nam neque necessitatibus est odio accusamus magni tempora
+        vero voluptas sunt sed omnis, aliquam qui? Accusamus laboriosam nisi
+        ratione laudantium dicta tenetur eveniet modi provident earum dolor,
+        quidem illum esse pariatur omnis facere, soluta distinctio reiciendis
+        neque. Pariatur recusandae eos eaque est hic magnam quidem eveniet
+        accusantium officia doloremque ut illum adipisci fugit veritatis
+        architecto, culpa, quod asperiores facere molestiae. Delectus dolore
+        deleniti molestiae inventore itaque suscipit nesciunt expedita modi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus modi sit
+        omnis repellat, tempora porro quis libero nemo ratione? Itaque
+        consectetur, rerum incidunt, nisi aliquid blanditiis quidem inventore
+        libero iusto nam neque necessitatibus est odio accusamus magni tempora
+        vero voluptas sunt sed omnis, aliquam qui? Accusamus laboriosam nisi
+        ratione laudantium dicta tenetur eveniet modi provident earum dolor,
+        quidem illum esse pariatur omnis facere, soluta distinctio reiciendis
+        neque. Pariatur recusandae eos eaque est hic magnam quidem eveniet
+        accusantium officia doloremque ut illum adipisci fugit veritatis
+        architecto, culpa, quod asperiores facere molestiae. Delectus dolore
+        deleniti molestiae inventore itaque suscipit nesciunt expedita modi.
+      </p>
+    </div>
+  );
+};
+
+export default About;
